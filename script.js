155d6503da4c70dab8394f656d94b75e50f2548a
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2023-12-29 23:53:47 +0100

    Added Scrolling Background Functionality to Script
    Updated script.js to include speed attribute and update method for the background object, enabling a scrolling effect. Also modified the draw method to redraw the image for continuous scrolling.

--- script.js
+++ script.js
@@ -90,11 +90,18 @@ window.addEventListener('load', function () {
             this.y = 0;
             this.width = 2400;
             this.height = 720;
+            this.speed = 7
 
 
         }
         draw(context) {
-            context.drawImage(this.image, this.x, this.y)
+            context.drawImage(this.image, this.x, this.y, this.width, this.height)
+            context.drawImage(this.image, this.x + this.width - this.speed, this.y, this.width, this.height)
+        }
+        update() {
+            this.x -= this.speed;
+            if (this.x < 0 - this.width) this.x = 0;
+
         }
     }
 
@@ -111,6 +118,7 @@ window.addEventListener('load', function () {
     function animate() {
         ctx.clearRect(0, 0, canvas.width, canvas.height)
         background.draw(ctx)
+        background.update()
         player.draw(ctx)
         player.update(input)
         requestAnimationFrame(animate);
