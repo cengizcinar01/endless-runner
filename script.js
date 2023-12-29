c1a38cf41820d92cd9222767d80d8f54278a989f
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2023-12-30 00:21:06 +0100

    Enhanced Game Animation and Control
    In this update, sprite animation and control elements were improved in the game script. The player and enemy objects were given frame and time properties to manage sprite animations. The update method for both objects was also enhanced to include sprite animation logic. The enemy spawning logic was adjusted to include a random interval for more dynamic gameplay. The player's update method now also considers delta time for smoother animation.

--- script.js
+++ script.js
@@ -41,18 +41,32 @@ window.addEventListener('load', function () {
             this.y = this.gameHeight - this.height;
             this.image = document.getElementById("playerImage")
             this.frameX = 0;
+            this.maxFrame = 8;
             this.frameY = 0;
+            this.fps = 20;
+            this.frameTimer = 0;
+            this.frameInterval = 1000/this.fps;
             this.speed = 0;
             this.vy = 0;
             this.weight = 1;
 
         }
         draw(context) {
-            context.fillStyle = "white";
-            context.fillRect(this.x, this.y, this.width, this.height);
+            //context.fillStyle = "white";
+            //context.fillRect(this.x, this.y, this.width, this.height);
             context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height)
         }
-        update(input) {
+        update(input, deltaTime) {
+            // sprite animation
+            if (this.frameTimer > this.frameInterval) {
+                if (this.frameX >= this.maxFrame) this.frameX = 0;
+                else this.frameX++;
+                this.frameTimer = 0;
+            } else {
+                this.frameTimer += deltaTime;
+            }
+
+            // controls
             if (input.keys.indexOf("ArrowRight") > -1) {
                 this.speed = 5;
             } else if (input.keys.indexOf("ArrowLeft") > -1) {
@@ -116,25 +130,38 @@ window.addEventListener('load', function () {
             this.x = this.gameWidth - 50;
             this.y = this.gameHeight - this.height;
             this.frameX = 0;
+            this.maxFrame = 5;
+            this.fps = 20;
+            this.frameTimer = 0;
+            this.frameInterval = 1000/this.fps;
+            this.speed = 8;
         }
         draw(context) {
             context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height)
         }
-        update() {
-            this.x--;
+        update(deltaTime) {
+            if (this.frameTimer > this.frameInterval) {
+                if (this.frameX >= this.maxFrame) this.frameX = 0;
+                else this.frameX++
+                this.frameTimer = 0;
+            } else {
+                this.frameTimer += deltaTime
+            }
+            this.x -= this.speed;
         }
     }
 
     function handleEnemies(deltaTime) {
-        if (enemyTimer > enemyInterval) {
+        if (enemyTimer > enemyInterval + randomEnemyInterval) {
             enemies.push(new Enemy(canvas.width, canvas.height))
+            randomEnemyInterval = Math.random() * 1000 + 500
             enemyTimer = 0;
         } else {
             enemyTimer += deltaTime;
         }
         enemies.forEach(enemy => {
             enemy.draw(ctx);
-            enemy.update()
+            enemy.update(deltaTime)
         })
     }
 
@@ -146,7 +173,8 @@ window.addEventListener('load', function () {
 
     let lastTime = 0;
     let enemyTimer = 0;
-    let enemyInterval = 1000;
+    let enemyInterval = 2000;
+    let randomEnemyInterval = Math.random() * 1000 + 500
 
     function animate(timeStamp) {
         const deltaTime = timeStamp - lastTime;
@@ -155,7 +183,7 @@ window.addEventListener('load', function () {
         background.draw(ctx)
         //background.update()
         player.draw(ctx)
-        player.update(input)
+        player.update(input, deltaTime)
         handleEnemies(deltaTime)
         requestAnimationFrame(animate);
     }
