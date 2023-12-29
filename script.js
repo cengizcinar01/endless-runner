c4e14fd59b3535e5d85c1d5095e922b3f6a3774c
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2023-12-29 20:51:10 +0100

    Updated Player Class and Removed Console Logs in Script.js
    The script.js file has been updated to include the constructor, draw, and update methods in the Player class. This allows the player's position and size to be defined and updated. The console.log statements have been removed from the InputHandler class. The animate function has also been updated to call the draw and update methods of the player class.

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ window.addEventListener('load', function () {
                 && this.keys.indexOf(e.key) === -1) {
                     this.keys.push(e.key);
                 }
-                console.log(e.key, this.keys);
             });
             window.addEventListener('keydown', (e) => {
                 if (e.key == 'ArrowDown' || 
@@ -25,12 +24,28 @@ window.addEventListener('load', function () {
                  {
                     this.keys.splice(this.keys.indexOf(e.key), 1);
                 }
-                console.log(e.key, this.keys);
             });
         }
     }
 
-    class Player {}
+    class Player {
+        constructor(gameWidth, gameHeight) {
+            this.gameWidth = gameWidth
+            this.gameHeight = gameHeight
+            this.width = 200;
+            this.height = 200;
+            this.x = 0;
+            this.y = this.gameHeight - this.height;
+
+        }
+        draw(context) {
+            context.fillStyle = "white";
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
+        update() {
+            this.x++
+        }
+    }
 
     class Backgroud {}
 
@@ -41,6 +56,12 @@ window.addEventListener('load', function () {
     function displayStatusText() {}
 
     const input = new InputHandler();
+    const player = new Player(canvas.width, canvas.height);
 
-    function animate() {}
+    function animate() {
+        player.draw(ctx)
+        player.update()
+        requestAnimationFrame(animate);
+    }
+    animate()
 });
