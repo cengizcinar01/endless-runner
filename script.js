25d5dfa91a201ab8045b7e7c66351848a6e7a109
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2023-12-30 00:51:57 +0100

    Updated Game Over Message and Collision Detection in Script.js
    This commit updates the script.js file to improve the game over message and collision detection. It removes the stroke and fill styles, and it changes the way distances are calculated for collision detection. It also adds a game over message that is displayed in the center of the canvas when the game is over.

--- script.js
+++ script.js
@@ -54,18 +54,13 @@ window.addEventListener('load', function () {
 
         }
         draw(context) {
-            context.fillStyle = "white";
-            context.strokeRect(this.x, this.y, this.width, this.height);
-            context.beginPath();
-            context.arc(this.x + this.width/2, this.y + this.height/2, this.width/2, 0, Math.PI * 2);
-            context.stroke();
             context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height)
         }
         update(input, deltaTime, enemies) {
             // collision detection
             enemies.forEach(enemy => {
-                const dx = enemy.x - this.x;
-                const dy = enemy.y - this.y;
+                const dx = (enemy.x + enemy.width/2) - (this.x + this.width/2);
+                const dy = (enemy.y + enemy.height/2) - (this.y + this.height/2);
                 const distance = Math.sqrt(dx * dx + dy * dy);
                 if (distance < enemy.width/2 + this.width/2) {
                     gameOver = true;
@@ -154,11 +149,6 @@ window.addEventListener('load', function () {
             this.markedForDeletion = false;
         }
         draw(context) {
-            context.strokeStyle = "white"
-            context.strokeRect(this.x, this.y, this.width, this.height)
-            context.beginPath();
-            context.arc(this.x + this.width/2, this.y + this.height/2, this.width/2, 0, Math.PI * 2);
-            context.stroke();
             context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height)
         }
         update(deltaTime) {
@@ -199,6 +189,13 @@ window.addEventListener('load', function () {
         context.fillText("Score: " + score, 20, 50);
         context.fillStyle = "white";
         context.fillText("Score: " + score, 22, 52);
+        if (gameOver) {
+            context.textAlign = "center"
+            context.fillStyle = "black";
+            context.fillText("GAME OVER, try again! ", canvas.width/2, 200);
+            context.fillStyle = "white";
+            context.fillText("GAME OVER, try again! ", canvas.width/2 + 2, 202);
+        }
     }
 
     const input = new InputHandler();
