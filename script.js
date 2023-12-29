a48a8dc989b9bc76cb99d66b4020bc772f6defa3
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2023-12-29 20:57:36 +0100

    Introduced Player Animation and Speed Control
    Added frameX, frameY, and speed properties to the player object for animation purposes. Also modified the draw and update functions to accommodate these changes.

--- script.js
+++ script.js
@@ -37,15 +37,18 @@ window.addEventListener('load', function () {
             this.x = 0;
             this.y = this.gameHeight - this.height;
             this.image = document.getElementById("playerImage")
+            this.frameX = 0;
+            this.frameY = 0;
+            this.speed = 0;
 
         }
         draw(context) {
             context.fillStyle = "white";
             context.fillRect(this.x, this.y, this.width, this.height);
-            context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height)
+            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height)
         }
-        update() {
-            this.x++
+        update(input) {
+            this.x += this.speed;
         }
     }
 
